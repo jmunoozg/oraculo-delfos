be13e17db0f5e8bc14e26923284d055f114c9d3f
Jaime Muñoz <jaime.munozgab@gmail.com>
2024-04-16 23:56:27 +0200

    Cambios en el botón y en media queries

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function mostrarPreguntas() {
     var preguntasContainer = document.getElementById("preguntas-container");
 
-    preguntasContainer.style.display = "block";
+    if (preguntasContainer.style.display === "none" || preguntasContainer.style.display === "") {
+        preguntasContainer.style.display = "block";
+    } else {
+        preguntasContainer.style.display = "none";
+    }
 }
+
+
